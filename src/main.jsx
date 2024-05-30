3926ce6662b9251da42dc4fcb296516b5977bbed
yousof-ali <yousofali293@gmail.com>
2024-05-30 15:11:35 +0600

    install react router

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path:"/about",
+    element:<div>This is form about</div>
+  },
+  
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
